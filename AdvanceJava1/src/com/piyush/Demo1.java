1b75c6e783973c571146129d873a19e63dff0aa2
Piyush kushwah <piyushkushwah002@gmail.com>
2026-02-11 16:48:39 +0530

    Add files via upload

---
+++ AdvanceJava1/src/com/piyush/Demo1.java
@@ -0,0 +1,34 @@
+package com.piyush;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Demo1 {
+
+    private static final String DBURL = "jdbc:mysql://localhost:3306/regex2";
+    private static final String DBUSERNAME ="root";
+    private static final String DBPASSWORD = "root";
+    
+    private static final String INSERTSQL = "INSERT INTO regex2 VALUES(3,'john','jaipur')";
+    
+    public static void main(String[] args) throws Exception {
+        
+        // 1.Load the Driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        // 2. Get the connection
+        Connection con = DriverManager.getConnection(DBURL,DBUSERNAME,DBPASSWORD);
+        
+        // 3. Create statement 
+        Statement stmt = con.createStatement();
+        
+        // 4. Execute and process the result 
+        int rowsaffected = stmt.executeUpdate(INSERTSQL);
+        System.out.println("Rows Inserted: " + rowsaffected);
+                 
+        // 5. Close the connection
+        con.close();
+        
+    }
+}
+
---
+++ AdvanceJava1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AdvanceJava1 {
+	requires java.sql;
+}
